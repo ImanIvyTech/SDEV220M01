2f12653afa2e748d16c99ae83df24ca9045024ed
ImanIvyTech <116987546+ImanIvyTech@users.noreply.github.com>
2022-10-31 20:44:13 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def m01lab():
+    # Use a breakpoint in the code line below to debug your script.
+    print('Hello World')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def m01assignment():
+    seconds_per_hour = 60 * 60
+    seconds_per_day = seconds_per_hour * 24
+    print(seconds_per_day / seconds_per_hour)
+    print(seconds_per_day // seconds_per_hour)
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
